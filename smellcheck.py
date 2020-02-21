e5062bde069b675caac4ea1bee13c4dcff81b60f
cmedvid <catherine.medvid@gmail.com>
2020-02-21 17:18:20 -0600

    initial commit with project files

---
+++ smellcheck.py
@@ -0,0 +1,21 @@
+from PyDictionary import PyDictionary
+import sys
+import serial
+
+port = '/dev/cu.usbserial-1410'
+ard = serial.Serial(port,9600,timeout=5)
+
+dict = PyDictionary()
+print("Welcome! Type a word, check your spelling, and ignore the initial warning message! :)")
+for line in sys.stdin:
+    input = line
+    if dict.meaning(input):
+        pass
+    else:
+        print("Wrong, you fool")
+        scent = 1
+        str(scent)
+        ard.write(scent)
+    scent = 0
+
+
